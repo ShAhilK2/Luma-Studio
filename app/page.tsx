21a3b4ebe9c6f44cbcdbe0245e86534cd325b74e
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-04-02 11:47:52 +0530

    feat: Implemented a Schema and generations Action and Update HomeHeroSection component and enhance utility functions; add new dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 import { Footer } from "@/components/Footer";
 import { GalleryShowcaseSection } from "@/components/GalleryShowcaseSection";
 import { GridBackground } from "@/components/GridBackground";
-import { HomeHeroSection } from "@/components/HomeHeroSection copy";
+import { HomeHeroSection } from "@/components/HomeHeroSection";
+
 import { HowItWorksSection } from "@/components/HowItWorksSection";
 import { PricingSection } from "@/components/PricingSection";
 
@@ -13,7 +14,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="min-h-screen bg-background p-3 sm:p-4 lg:p-5">
-    <HomeHeroSection />
+    <HomeHeroSection/>
     <GalleryShowcaseSection />
     <HowItWorksSection />
    <PricingSection />
